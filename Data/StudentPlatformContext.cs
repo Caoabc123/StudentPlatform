5a16bb992738a59646262d6efcc48a695a54b5ea
Fredrik Lindroth <fredrik.lindroth@sprinto.se>
2020-12-15 14:22:26 +0100

    Add project files.

---
+++ Data/StudentPlatformContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentPlatform.Data
+{
+    public class StudentPlatformContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        public StudentPlatformContext(DbContextOptions<StudentPlatformContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            // skapa composite primary key
+            // studentId och courseId är tillsammans alltså primary key
+            modelbuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            // säga till EF vad relationen mellan studentcourse och student är
+            // sätta att studentId är foreign key 
+            modelbuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentCourses)
+                .HasForeignKey(sc => sc.StudentId);
+                //.OnDelete(DeleteBehavior.Restrict)
+                
+
+            // säga till EF vad relationen mellan studentcourse och course är
+            // sätta att courseId är foreign key 
+            modelbuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentCourses)
+                .HasForeignKey(sc => sc.CourseId);
+                //.OnDelete(DeleteBehavior.Restrict)
+        }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentPlatform.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Level { get; set; }
+        public int? TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+        public ICollection<StudentCourse> StudentCourses { get; set; }
+    }
+}
